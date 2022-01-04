8143609abe9d8f2c20863cf0b0e2115724e3b464
dasbd72 <twbd723@gmail.com>
2022-01-04 13:58:58 +0800

    Update structure

--- README.md
+++ README.md
@@ -4,7 +4,16 @@
 
 [float 32 calculation](https://en.wikipedia.org/wiki/Single-precision_floating-point_format)
 
-## Memory Usage (tot : 2 * 1024 * 1024 = 2097152)
+## Memory Usage (tot : 2 _ 1024 _ 1024 = 2097152)
 
-- Picture : 12 * 76800 = 921600
-- 
+- Picture : 12 \* 76800 = 921600
+-
+
+## Dependency
+
+- Top.v
+  - Sudoku.v
+  - mouse/Mouse.v
+    - mouse/MouseCtl.vhd
+      - mouse/Ps2Interface.vhd
+    - mouse/MouseDisplay.vhd
---
+++ src/utilities/Clock_Divider.v
@@ -0,0 +1,28 @@
+`timescale 1ps/1ps
+module Clock_Divider #(
+    parameter length = 4
+    ) (
+    input wire clk,
+    input wire rst_n,
+    output reg dclk
+    );
+    
+    reg nxt_dclk;
+    reg [31:0] cnt_cycle;
+
+    // Posedge Trigger
+    always @(posedge clk) begin
+        if(~rst_n) begin
+            cnt_cycle <= 32'b1;
+            dclk <= 1'b0;
+        end else begin
+            cnt_cycle <= ((cnt_cycle == length) ? 32'b1 : cnt_cycle + 32'b1);
+            dclk <= nxt_dclk;
+        end
+    end
+
+    always @(*) begin
+        if(cnt_cycle == length) nxt_dclk = 1'b1;
+        else nxt_dclk = 1'b0;
+    end
+endmodule
