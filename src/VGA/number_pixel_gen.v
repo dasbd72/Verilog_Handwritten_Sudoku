d72ba93de856c0040b506c7f91717aa662e72d96
yuxunnn <yuxun90140@gmail.com>
2022-01-04 14:20:45 +0800

    MOved

---
+++ src/VGA/number_pixel_gen.v
@@ -0,0 +1,19 @@
+module number_pixel_gen(
+    input [9:0] h_cnt,
+    input [9:0] v_cnt,
+    input [9:0] index,
+    input [10:0] number,
+    output reg [11:0] pixel
+    );
+
+    parameter size = 52;
+    parameter O = 12'b1111_1111_1111; // White
+    parameter K = 12'b0000_0000_0000; // Black
+
+    wire [9:0] row = index / 9 * 52;
+    wire [9:0] col = index % 9 * 52;
+
+    always @(*) begin
+        if (h_cnt <= row)
+    end
+endmodule
