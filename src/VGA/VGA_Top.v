82da3ef97d21d69b500f68ef48697ce07c53e0d4
yuxunnn <yuxun90140@gmail.com>
2022-01-05 19:57:27 +0800

    mouse

--- src/VGA/VGA_Top.v
+++ src/VGA/VGA_Top.v
@@ -1,33 +1,42 @@
 module VGA_Top(
 	input clk,
-	input [1:0] State,
 	input rst,
 	input change,
 	output [3:0] vgaRed,
 	output [3:0] vgaGreen,
 	output [3:0] vgaBlue,
 	output hsync,
-	output vsync
+	output vsync,
+	inout PS2_CLK,
+	inout PS2_DATA
 	);
 
 	wire [11:0] din_game;
 	wire din_menu;
-	wire clk_25MHz;
-	wire clk_22;
+	wire clk_25MHz, clk_22;
 	wire valid;
 	wire [9:0] h_cnt;  // 640
 	wire [9:0] v_cnt;  // 480
 	wire [16:0] pixel_addr;
-	reg [11:0] pixel;
 	wire pixel_menu;
 	wire [11:0] pixel_game;
 	wire [11:0] pixel_number;
 	wire [11:0] pixel_out;
+	reg [11:0] pixel;
+
+	wire enable_mouse_display;
+	wire [9:0] MOUSE_X_POS , MOUSE_Y_POS;
+    wire MOUSE_LEFT , MOUSE_MIDDLE , MOUSE_RIGHT , MOUSE_NEW_EVENT;
+    wire [3:0] mouse_cursor_red , mouse_cursor_green , mouse_cursor_blue;
+    wire [11:0] mouse_pixel = {mouse_cursor_red, mouse_cursor_green, mouse_cursor_blue};
+
 
 	assign pixel_addr = ((h_cnt>>1) + 320 * (v_cnt>>1)) % 76800;
 	assign {vgaRed, vgaGreen, vgaBlue} = (valid==1'b1) ? pixel : 12'h0;
 	always @(*) begin
-		if (change) begin
+		if (enable_mouse_display) begin
+			pixel = mouse_pixel;
+		end else if (change) begin
 			pixel = pixel_game;
 		end else begin
 			if (pixel_menu == 0) begin
@@ -76,6 +85,24 @@ module VGA_Top(
 		.valid(valid),
 		.h_cnt(h_cnt),
 		.v_cnt(v_cnt)
+	);
+
+	Mouse mouse_inst(
+		.clk(clk),
+		.h_cntr_reg(h_cnt),
+		.v_cntr_reg(v_cnt),
+		.enable_mouse_display(enable_mouse_display),
+		.MOUSE_X_POS(MOUSE_X_POS),
+		.MOUSE_Y_POS(MOUSE_Y_POS),
+		.MOUSE_LEFT(MOUSE_LEFT),
+		.MOUSE_MIDDLE(MOUSE_MIDDLE),
+		.MOUSE_RIGHT(MOUSE_RIGHT),
+		.MOUSE_NEW_EVENT(MOUSE_NEW_EVENT),
+		.mouse_cursor_red(mouse_cursor_red),
+		.mouse_cursor_green(mouse_cursor_green),
+		.mouse_cursor_blue(mouse_cursor_blue),
+		.PS2_CLK(PS2_CLK),
+		.PS2_DATA(PS2_DATA)
 	);
       
 endmodule
