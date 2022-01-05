f4a34b3141c6f65564bc08d65b1915426613d50d
yuxunnn <yuxun90140@gmail.com>
2022-01-06 02:06:53 +0800

    sleep sudoku

--- src/Sudoku.v
+++ src/Sudoku.v
@@ -1,82 +1,107 @@
-module Sudoku_Solver (clk, start, reset, data, valid, done);
+module Sudoku_Solver (clk, reset, start, read, block_pos, data, init_board, board, valid, done);
     input clk;
     input reset;
     input start;
-    input [10:0] data;
+    input read;
+    input [6:0] block_pos;
+    input [3:0] data;
+    input [9*9*4-1:0]  init_board;
+    output reg [9*9*4-1:0] board;
     output reg valid;
     output reg done;
     
-    // ==========================
-    // Store the current 9x9 board 
-    // ==========================
-    reg [10:0] board [0:8][0:8]; // The most significant bit [10] indicates empty
-    reg [10:0] data_in;
+    parameter SIZE = 9*9*4;
 
     // ==========================
     // State parameters
     // ==========================
     reg State, State_next;
-    parameter Read =  0;
-    parameter Finish = 1;
+    parameter WAIT =  0;
+    parameter READ = 1;
+    reg [3:0] data_next;
 
     // ==========================
     // The current reading position
     // ==========================
-    reg  [6:0] grid_count, grid_count_next;
-    assign row = grid_count / 9;
-    assign col = grid_count % 9;
+    assign row = block_pos / 9;
+    assign col = block_pos - row * 9;
 
     // ==========================
     // Check the sudoku is valid
     // ==========================
     // ------------- Row ------------- 
-    wire [0:8] row_vaild;
-    Check_Valid CV_row0(row_vaild[0], board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7], board[0][8]);
-    Check_Valid CV_row1(row_vaild[1], board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7], board[1][8]);
-    Check_Valid CV_row2(row_vaild[2], board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7], board[2][8]);
-    Check_Valid CV_row3(row_vaild[3], board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7], board[3][8]);
-    Check_Valid CV_row4(row_vaild[4], board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7], board[4][8]);
-    Check_Valid CV_row5(row_vaild[5], board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7], board[5][8]);
-    Check_Valid CV_row6(row_vaild[6], board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7], board[6][8]);
-    Check_Valid CV_row7(row_vaild[7], board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7], board[7][8]);
-    Check_Valid CV_row8(row_vaild[8], board[8][0], board[8][1], board[8][2], board[8][3], board[8][4], board[8][5], board[8][6], board[8][7], board[8][8]);
+    wire [0:8] row_valid;
+    Check_Valid CV_row0(row_valid[0], board[SIZE-1-:4], board[SIZE-5-:4], board[SIZE-9-:4], board[SIZE-13-:4], 
+                        board[SIZE-17-:4], board[SIZE-21-:4], board[SIZE-25-:4], board[SIZE-29-:4], board[SIZE-33-:4]); 
+    Check_Valid CV_row1(row_valid[1], board[SIZE-37-:4], board[SIZE-41-:4], board[SIZE-45-:4], board[SIZE-49-:4], 
+                        board[SIZE-53-:4], board[SIZE-57-:4], board[SIZE-61-:4], board[SIZE-65-:4], board[SIZE-69-:4]); 
+    Check_Valid CV_row2(row_valid[2], board[SIZE-73-:4], board[SIZE-77-:4], board[SIZE-81-:4], board[SIZE-85-:4], 
+                        board[SIZE-89-:4], board[SIZE-93-:4], board[SIZE-97-:4], board[SIZE-101-:4], board[SIZE-105-:4]);
+    Check_Valid CV_row3(row_valid[3], board[SIZE-109-:4], board[SIZE-113-:4], board[SIZE-117-:4], board[SIZE-121-:4], 
+                        board[SIZE-125-:4], board[SIZE-129-:4], board[SIZE-133-:4], board[SIZE-137-:4], board[SIZE-141-:4]);
+    Check_Valid CV_row4(row_valid[4], board[SIZE-145-:4], board[SIZE-149-:4], board[SIZE-153-:4], board[SIZE-157-:4], 
+                        board[SIZE-161-:4], board[SIZE-165-:4], board[SIZE-169-:4], board[SIZE-173-:4], board[SIZE-177-:4]);
+    Check_Valid CV_row5(row_valid[5], board[SIZE-181-:4], board[SIZE-185-:4], board[SIZE-189-:4], board[SIZE-193-:4], 
+                        board[SIZE-197-:4], board[SIZE-201-:4], board[SIZE-205-:4], board[SIZE-209-:4], board[SIZE-213-:4]);
+    Check_Valid CV_row6(row_valid[6], board[SIZE-217-:4], board[SIZE-221-:4], board[SIZE-225-:4], board[SIZE-229-:4], 
+                        board[SIZE-233-:4], board[SIZE-237-:4], board[SIZE-241-:4], board[SIZE-245-:4], board[SIZE-249-:4]);
+    Check_Valid CV_row7(row_valid[7], board[SIZE-253-:4], board[SIZE-257-:4], board[SIZE-261-:4], board[SIZE-265-:4], 
+                        board[SIZE-269-:4], board[SIZE-273-:4], board[SIZE-277-:4], board[SIZE-281-:4], board[SIZE-285-:4]);
+    Check_Valid CV_row8(row_valid[8], board[SIZE-289-:4], board[SIZE-293-:4], board[SIZE-297-:4], board[SIZE-301-:4], 
+                        board[SIZE-305-:4], board[SIZE-309-:4], board[SIZE-313-:4], board[SIZE-317-:4], board[SIZE-321-:4]);
     // ------------- Column ------------- 
-    wire [0:8] col_vaild;
-    Check_Valid CV_col0(col_vaild[0], board[0][0], board[1][0], board[2][0], board[3][0], board[4][0], board[5][0], board[6][0], board[7][0], board[8][0]);
-    Check_Valid CV_col1(col_vaild[1], board[0][1], board[1][1], board[2][1], board[3][1], board[4][1], board[5][1], board[6][1], board[7][1], board[8][1]);
-    Check_Valid CV_col2(col_vaild[2], board[0][2], board[1][2], board[2][2], board[3][2], board[4][2], board[5][2], board[6][2], board[7][2], board[8][2]);
-    Check_Valid CV_col3(col_vaild[3], board[0][3], board[1][3], board[2][3], board[3][3], board[4][3], board[5][3], board[6][3], board[7][3], board[8][3]);
-    Check_Valid CV_col4(col_vaild[4], board[0][4], board[1][4], board[2][4], board[3][4], board[4][4], board[5][4], board[6][4], board[7][4], board[8][4]);
-    Check_Valid CV_col5(col_vaild[5], board[0][5], board[1][5], board[2][5], board[3][5], board[4][5], board[5][5], board[6][5], board[7][5], board[8][5]);
-    Check_Valid CV_col6(col_vaild[6], board[0][6], board[1][6], board[2][6], board[3][6], board[4][6], board[5][6], board[6][6], board[7][6], board[8][6]);
-    Check_Valid CV_col7(col_vaild[7], board[0][7], board[1][7], board[2][7], board[3][7], board[4][7], board[5][7], board[6][7], board[7][7], board[8][7]);
-    Check_Valid CV_col8(col_vaild[8], board[0][8], board[1][8], board[2][8], board[3][8], board[4][8], board[5][8], board[6][8], board[7][8], board[8][8]);
+    wire [0:8] col_valid;
+    Check_Valid CV_col0(col_valid[0], board[SIZE-1-:4], board[SIZE-37-:4], board[SIZE-73-:4], board[SIZE-109-:4], 
+                        board[SIZE-145-:4], board[SIZE-181-:4], board[SIZE-217-:4], board[SIZE-253-:4], board[SIZE-289-:4]);
+    Check_Valid CV_col1(col_valid[1], board[SIZE-5-:4], board[SIZE-41-:4], board[SIZE-77-:4], board[SIZE-113-:4], 
+                        board[SIZE-149-:4], board[SIZE-185-:4], board[SIZE-221-:4], board[SIZE-257-:4], board[SIZE-293-:4]);
+    Check_Valid CV_col2(col_valid[2], board[SIZE-9-:4], board[SIZE-45-:4], board[SIZE-81-:4], board[SIZE-117-:4], 
+                        board[SIZE-153-:4], board[SIZE-189-:4], board[SIZE-225-:4], board[SIZE-261-:4], board[SIZE-297-:4]);
+    Check_Valid CV_col3(col_valid[3], board[SIZE-13-:4], board[SIZE-49-:4], board[SIZE-85-:4], board[SIZE-121-:4], 
+                        board[SIZE-157-:4], board[SIZE-193-:4], board[SIZE-229-:4], board[SIZE-265-:4], board[SIZE-301-:4]);
+    Check_Valid CV_col4(col_valid[4], board[SIZE-17-:4], board[SIZE-53-:4], board[SIZE-89-:4], board[SIZE-125-:4], 
+                        board[SIZE-161-:4], board[SIZE-197-:4], board[SIZE-233-:4], board[SIZE-269-:4], board[SIZE-305-:4]);
+    Check_Valid CV_col5(col_valid[5], board[SIZE-21-:4], board[SIZE-57-:4], board[SIZE-93-:4], board[SIZE-129-:4], 
+                        board[SIZE-165-:4], board[SIZE-201-:4], board[SIZE-237-:4], board[SIZE-273-:4], board[SIZE-309-:4]);
+    Check_Valid CV_col6(col_valid[6], board[SIZE-25-:4], board[SIZE-61-:4], board[SIZE-97-:4], board[SIZE-133-:4], 
+                        board[SIZE-169-:4], board[SIZE-205-:4], board[SIZE-241-:4], board[SIZE-277-:4], board[SIZE-313-:4]);
+    Check_Valid CV_col7(col_valid[7], board[SIZE-29-:4], board[SIZE-65-:4], board[SIZE-101-:4], board[SIZE-137-:4], 
+                        board[SIZE-173-:4], board[SIZE-209-:4], board[SIZE-245-:4], board[SIZE-281-:4], board[SIZE-317-:4]);
+    Check_Valid CV_col8(col_valid[8], board[SIZE-33-:4], board[SIZE-69-:4], board[SIZE-105-:4], board[SIZE-141-:4], 
+                        board[SIZE-177-:4], board[SIZE-213-:4], board[SIZE-249-:4], board[SIZE-285-:4], board[SIZE-321-:4]);
     // ------------- Block ------------- 
     wire [0:8] blk_valid;
-    Check_Valid CV_blk0(blk_valid[0], board[0][0], board[0][1], board[0][2], board[1][0], board[1][1], board[1][2], board[2][0], board[2][1], board[2][2]);
-    Check_Valid CV_blk1(blk_valid[1], board[0][3], board[0][4], board[0][5], board[1][3], board[1][4], board[1][5], board[2][3], board[2][4], board[2][5]);
-    Check_Valid CV_blk2(blk_valid[2], board[0][6], board[0][7], board[0][8], board[1][6], board[1][7], board[1][8], board[2][6], board[2][7], board[2][8]);
-    Check_Valid CV_blk3(blk_valid[3], board[3][0], board[3][1], board[3][2], board[4][0], board[4][1], board[4][2], board[5][0], board[5][1], board[5][2]);
-    Check_Valid CV_blk4(blk_valid[4], board[3][3], board[3][4], board[3][5], board[4][3], board[4][4], board[4][5], board[5][3], board[5][4], board[5][5]);
-    Check_Valid CV_blk5(blk_valid[5], board[3][6], board[3][7], board[3][8], board[4][6], board[4][7], board[4][8], board[5][6], board[5][7], board[5][8]);
-    Check_Valid CV_blk6(blk_valid[6], board[6][0], board[6][1], board[6][2], board[7][0], board[7][1], board[7][2], board[8][0], board[8][1], board[8][2]);
-    Check_Valid CV_blk7(blk_valid[7], board[6][3], board[6][4], board[6][5], board[7][3], board[7][4], board[7][5], board[8][3], board[8][4], board[8][5]);
-    Check_Valid CV_blk8(blk_valid[8], board[6][6], board[6][7], board[6][8], board[7][6], board[7][7], board[7][8], board[8][6], board[8][7], board[8][8]);
+    Check_Valid CV_blk0(blk_valid[0], board[SIZE-1-:4], board[SIZE-5-:4], board[SIZE-9-:4], board[SIZE-37-:4], 
+                        board[SIZE-41-:4], board[SIZE-45-:4], board[SIZE-73-:4], board[SIZE-77-:4], board[SIZE-81-:4]);
+    Check_Valid CV_blk1(blk_valid[1], board[SIZE-13-:4], board[SIZE-17-:4], board[SIZE-21-:4], board[SIZE-49-:4], 
+                        board[SIZE-53-:4], board[SIZE-57-:4], board[SIZE-85-:4], board[SIZE-89-:4], board[SIZE-93-:4]);
+    Check_Valid CV_blk2(blk_valid[2], board[SIZE-25-:4], board[SIZE-29-:4], board[SIZE-33-:4], board[SIZE-61-:4], 
+                        board[SIZE-65-:4], board[SIZE-69-:4], board[SIZE-97-:4], board[SIZE-101-:4], board[SIZE-105-:4]);
+    Check_Valid CV_blk3(blk_valid[3], board[SIZE-109-:4], board[SIZE-113-:4], board[SIZE-117-:4], board[SIZE-145-:4], 
+                        board[SIZE-149-:4], board[SIZE-153-:4], board[SIZE-181-:4], board[SIZE-185-:4], board[SIZE-189-:4]);
+    Check_Valid CV_blk4(blk_valid[4], board[SIZE-121-:4], board[SIZE-125-:4], board[SIZE-129-:4], board[SIZE-157-:4], 
+                        board[SIZE-161-:4], board[SIZE-165-:4], board[SIZE-193-:4], board[SIZE-197-:4], board[SIZE-201-:4]);
+    Check_Valid CV_blk5(blk_valid[5], board[SIZE-133-:4], board[SIZE-137-:4], board[SIZE-141-:4], board[SIZE-169-:4], 
+                        board[SIZE-173-:4], board[SIZE-177-:4], board[SIZE-205-:4], board[SIZE-209-:4], board[SIZE-213-:4]);
+    Check_Valid CV_blk6(blk_valid[6], board[SIZE-217-:4], board[SIZE-221-:4], board[SIZE-225-:4], board[SIZE-253-:4], 
+                        board[SIZE-257-:4], board[SIZE-261-:4], board[SIZE-289-:4], board[SIZE-293-:4], board[SIZE-297-:4]);
+    Check_Valid CV_blk7(blk_valid[7], board[SIZE-229-:4], board[SIZE-233-:4], board[SIZE-237-:4], board[SIZE-265-:4], 
+                        board[SIZE-269-:4], board[SIZE-273-:4], board[SIZE-301-:4], board[SIZE-305-:4], board[SIZE-309-:4]);
+    Check_Valid CV_blk8(blk_valid[8], board[SIZE-241-:4], board[SIZE-245-:4], board[SIZE-249-:4], board[SIZE-277-:4], 
+                        board[SIZE-281-:4], board[SIZE-285-:4], board[SIZE-313-:4], board[SIZE-317-:4], board[SIZE-321-:4]);
     // ------------- Board ------------- 
-    wire board_valid = (row_vaild == 9'b111111111) & (col_vaild == 9'b111111111) & (blk_valid == 9'b111111111);
+    wire board_valid = (row_valid == 9'b111111111) & (col_valid == 9'b111111111) & (blk_valid == 9'b111111111);
 
     // ==========================
     // Clock update
     // ==========================
     always @(posedge clk) begin
         if (reset) begin
-            State <= Read;
-            board[row][col] <= board[row][col];
-            grid_count <= 0;
+            State <= WAIT;
+            board <= 0;
         end else begin    
             State <= State_next;
-            board[row][col] <= data_in;
-            grid_count <= grid_count_next;
+            board[SIZE-(1+row*9*4+col*4)-:4] <= data_next;
         end
     end
 
@@ -85,90 +110,48 @@ module Sudoku_Solver (clk, start, reset, data, valid, done);
     // ==========================
     always @(*) begin
         case (State)
-            Read: begin
-                /* Start reading data one by one */
-                if (start) begin
-                    /* Fetch the input data */
-                    data_in = data;
-                    /* Finish read the last grid */
-                    if (grid_count == 81 - 1) begin
-                        State_next = Finish;
-                        grid_count_next = 0;
-                    /* Read the next grid */
-                    end else begin
-                        State_next = Read;
-                        grid_count_next = grid_count + 1;
-                    end
-                    valid = 0;
+            WAIT: begin
+                if (read) begin
                     done = 0;
+                    State_next = READ;
+                    data_next = data;
                 end else begin
-                    valid = valid;
                     done = done;
-                    data_in = board[row][col];
-                    State_next = Read;
-                    grid_count_next = grid_count;
+                    State_next = WAIT;
+                    data_next = data_next;
                 end
+                State_next = WAIT;
             end
-            Finish: begin
-                if (start) State_next = Read;
-                else State_next = Finish;
-                data_in = board[row][col];
-                grid_count_next = grid_count_next;
-                valid = board_valid;
+            READ: begin
                 done = 1;
+                State_next = WAIT;
+                data_next = data_next;
             end
             default: begin
-                valid = 0;
                 done = 0;
-                data_in = board[row][col];
-                State_next = Read;
-                grid_count_next = 0;
+                State_next = WAIT;
+                data_next = data_next;
             end
         endcase
     end
     
 endmodule
 
-module Check_Valid (valid, data0, data1, data2, data3, data4, data5, data6, data7, data8);
-    input [10:0] data0, data1, data2;
-    input [10:0] data3, data4, data5;
-    input [10:0] data6, data7, data8;
+module Check_Valid (valid, d0, d1, d2, d3, d4, d5, d6, d7, d8);
+    input [3:0] d0, d1, d2, d3, d4, d5, d6, d7, d8;
     output valid;
 
-    wire [0:8] have_same;
-    wire [0:8] valid_data;
-
-    Check_Same cs0(have_same[0], data0, data1, data2, data3, data4, data5, data6, data7, data8);
-    Check_Same cs1(have_same[1], data1, data0, data2, data3, data4, data5, data6, data7, data8);
-    Check_Same cs2(have_same[2], data2, data1, data0, data3, data4, data5, data6, data7, data8);
-    Check_Same cs3(have_same[3], data3, data1, data2, data0, data4, data5, data6, data7, data8);
-    Check_Same cs4(have_same[4], data4, data1, data2, data3, data0, data5, data6, data7, data8);
-    Check_Same cs5(have_same[5], data5, data1, data2, data3, data4, data0, data6, data7, data8);
-    Check_Same cs6(have_same[6], data6, data1, data2, data3, data4, data5, data0, data7, data8);
-    Check_Same cs7(have_same[7], data7, data1, data2, data3, data4, data5, data6, data0, data8);
-    Check_Same cs8(have_same[8], data8, data1, data2, data3, data4, data5, data6, data7, data0);
+    wire [8:0] valid_data;
 
     assign valid = valid_data == 9'b1_1111_1111;
-    assign valid_data[0] = data0[10] | (!data0[10] & !have_same[0]);
-    assign valid_data[1] = data1[10] | (!data1[10] & !have_same[1]);
-    assign valid_data[2] = data2[10] | (!data2[10] & !have_same[2]);
-    assign valid_data[3] = data3[10] | (!data3[10] & !have_same[3]);
-    assign valid_data[4] = data4[10] | (!data4[10] & !have_same[4]);
-    assign valid_data[5] = data5[10] | (!data5[10] & !have_same[5]);
-    assign valid_data[6] = data6[10] | (!data6[10] & !have_same[6]);
-    assign valid_data[7] = data7[10] | (!data7[10] & !have_same[7]);
-    assign valid_data[8] = data8[10] | (!data8[10] & !have_same[8]);
+    assign valid_data[0] = d0 != 0 & !(d0 == d1 | d0 == d2 | d0 == d3 | d0 == d4 | d0 == d5 | d0 == d6 | d0 == d7 | d0 == d8);
+    assign valid_data[1] = d1 != 0 & !(d1 == d0 | d1 == d2 | d1 == d3 | d1 == d4 | d1 == d5 | d1 == d6 | d1 == d7 | d1 == d8);
+    assign valid_data[2] = d2 != 0 & !(d2 == d1 | d2 == d0 | d2 == d3 | d2 == d4 | d2 == d5 | d2 == d6 | d2 == d7 | d2 == d8);
+    assign valid_data[3] = d3 != 0 & !(d3 == d1 | d3 == d2 | d3 == d0 | d3 == d4 | d3 == d5 | d3 == d6 | d3 == d7 | d3 == d8);
+    assign valid_data[4] = d4 != 0 & !(d4 == d1 | d4 == d2 | d4 == d3 | d4 == d0 | d4 == d5 | d4 == d6 | d4 == d7 | d4 == d8);
+    assign valid_data[5] = d5 != 0 & !(d5 == d1 | d5 == d2 | d5 == d3 | d5 == d4 | d5 == d0 | d5 == d6 | d5 == d7 | d5 == d8);
+    assign valid_data[6] = d6 != 0 & !(d6 == d1 | d6 == d2 | d6 == d3 | d6 == d4 | d6 == d5 | d6 == d0 | d6 == d7 | d6 == d8);
+    assign valid_data[7] = d7 != 0 & !(d7 == d1 | d7 == d2 | d7 == d3 | d7 == d4 | d7 == d5 | d7 == d6 | d7 == d0 | d7 == d8);
+    assign valid_data[8] = d8 != 0 & !(d8 == d1 | d8 == d2 | d8 == d3 | d8 == d4 | d8 == d5 | d8 == d6 | d8 == d7 | d8 == d0);
     
-endmodule
-
-module Check_Same (same, target, data1, data2, data3, data4, data5, data6, data7, data8);
-    input [10:0] target, data1, data2;
-    input [10:0] data3, data4, data5;
-    input [10:0] data6, data7, data8;
-    output same;
-
-    assign same = target[9:0] == data1[9:0] | target[9:0] == data2[9:0] | target[9:0] == data3[9:0] |
-                  target[9:0] == data4[9:0] | target[9:0] == data5[9:0] | target[9:0] == data6[9:0] |
-                  target[9:0] == data7[9:0] | target[9:0] == data8[9:0];
-
 endmodule
