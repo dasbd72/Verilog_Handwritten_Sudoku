48fde602103dd5f5841b7d52eed12deefdd57b69
yuxunnn <yuxun90140@gmail.com>
2022-01-10 21:46:23 +0800

    hi

--- src/Top.v
+++ src/Top.v
@@ -3,26 +3,22 @@ module Top #(
     parameter t = 1
     ) (
     input clk,
-    input rst,
+    input btnU,
     output [3:0] vgaRed,
     output [3:0] vgaGreen,
     output [3:0] vgaBlue,
     output hsync,
     output vsync,
-    output [3:0] AN,
-    output [6:0] SEG,
+    // output [3:0] AN,
+    // output [6:0] SEG,
     inout PS2_CLK,
-    inout PS2_DATA,
+    inout PS2_DATA
     /* Board Communication (1 + 81 times) */
-    input request,
-    input ack_in,
-    output ack_out,
-    output [3:0] data,
-    output valid
+    // input request,
+    // input ack_in,
+    // output ack_out,
+    // output [3:0] data,
+    // output valid
     );
 
-    localparam SMENU = 4'd0;
-    localparam SGAME = 4'd1;
-    reg [3:0] state, next_state;
-    
 endmodule
