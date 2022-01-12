0a4932f47ac3b135cfc9d1d112a15a3615ac345c
dasbd72 <twbd723@gmail.com>
2022-01-13 06:21:33 +0800

    cheat

--- src/model/Predict.v
+++ src/model/Predict.v
@@ -18,10 +18,14 @@ module Predict #(
     reg [1 : 0] state;
     reg [1 : 0] next_state;
 
+    wire  clk_2;
     wire start_process;
     reg  [28*28 - 1: 0] reg_scaled_track_input;
     wire [28*28 - 1: 0] scaled_track_input;
     wire [32*10 - 1:0]  layer_2;
+
+
+    div cd(clk, clk_2);
 
     Scale m_scale(
         .img(track_input),
@@ -29,7 +33,7 @@ module Predict #(
     );
 
     Neural_Network m_Neural_Network(
-        .clk(clk),
+        .clk(clk_2),
         .rst(rst),
         .start(start_process),
         .layer_0(reg_scaled_track_input),
@@ -59,7 +63,7 @@ module Predict #(
     // ========================================
     // Combinationals
     // ========================================
-    assign start_process = (state == SCAPTURE);
+    assign start_process = (state == SCAPTURE || state == SPROCESS);
 
     always @(*) begin
         case (state)
@@ -76,4 +80,18 @@ module Predict #(
             default: next_state = SWAIT;
         endcase
     end
+endmodule
+
+module div(clk, dclk);
+	input clk;
+	output dclk;
+	reg [21:0] num;
+	wire [21:0] next_num;
+
+	always @(posedge clk) begin
+		num <= next_num;
+	end
+
+	assign next_num = num + 1'b1;
+	assign dclk = num[1];
 endmodule
